50e86436344d1c2345ad441ba734d9f471121d3c
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2022-10-07 08:21:29 +0900

    2022-10-07 DOKYUN Basic v0.9 일반 로그인 api ing

--- src/main/java/com/sparos/uniquone/msauserservice/users/controller/UserController.java
+++ src/main/java/com/sparos/uniquone/msauserservice/users/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.sparos.uniquone.msauserservice.users.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparos.uniquone.msauserservice.users.domain.Users;
+import com.sparos.uniquone.msauserservice.users.dto.user.UserChatResponseDto;
+import com.sparos.uniquone.msauserservice.users.dto.user.UserDto;
+import com.sparos.uniquone.msauserservice.users.dto.user.UserPwDto;
+import com.sparos.uniquone.msauserservice.users.dto.user.UserUpdateNickDto;
+import com.sparos.uniquone.msauserservice.users.repository.UserRepository;
+import com.sparos.uniquone.msauserservice.users.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class UserController {
-
     private final UserRepository userRepository;
 
     private final UserService userService;
@@ -21,9 +31,18 @@ public class UserController {
 
     // 채팅 - 유저 정보 요청 API
     @GetMapping("/chat/userInfo/{userId}")
-    public UserChatResponseDto chatUserInfo(@PathVariable("userId") Long userId){
+    public UserChatResponseDto chatUserInfo(@PathVariable("userId") Long userId) {
         System.err.println("냐냐냐냐");
         System.err.println("userId" + userId);
+
+        Users user = userRepository.findById(userId).get();
+
+        return UserChatResponseDto.builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .build();
+
+    }
     //회원 정보 조회
     @GetMapping
     public ResponseEntity<UserDto> findUserByAuthToken(HttpServletRequest request){
@@ -49,12 +68,4 @@ public class UserController {
 
 
 
-
-        Users user = userRepository.findById(userId).get();
-
-        return UserChatResponseDto.builder()
-                .userId(user.getId())
-                .nickname(user.getNickname())
-                .build();
-    }
 }
